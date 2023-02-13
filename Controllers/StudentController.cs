e4826931f5d9b2982db508153461c49b648adfbf
korzhinav <korzhinav@example.com>
2023-02-13 22:32:57 +0500

    Студентики добавляются

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using journalapp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 
 namespace journalapp.Controllers
@@ -43,8 +44,21 @@ namespace journalapp.Controllers
         }
 
         public IActionResult Create()
-        {
+        { IEnumerable<SelectListItem> GroupsDropDown=_DBcontext.Groups.Select(i => new SelectListItem{
+                Text=i.Id,
+                Value=i.Id
+            });
+            ViewBag.GroupsDropDown=GroupsDropDown;
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student newsStudent)
+        {
+ _DBcontext.Students.Add(newsStudent);
+            _DBcontext.SaveChanges();
+            return RedirectToAction("StudentList");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
